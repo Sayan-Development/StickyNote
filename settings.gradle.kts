8a54707573f8453f94f7de9964c19d65ca9d5b0d
Mohamad82Bz <mohamad.hossein82.bz@gmail.com>
2024-03-23 20:00:41 +0330

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+
+        // Used for the gradle plugin
+        maven("https://repo.screamingsandals.org/public")
+    }
+}
+
+rootProject.name = "StickyNote"
+
+/*include(":core")
+include(":paper")
+include(":bukkit")
+include(
+    ":database:core",
+    ":database:bukkit",
+    ":database:velocity"
+)
+include("bukkit-database")
+include("velocity-database")*/
