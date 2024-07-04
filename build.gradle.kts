f2693c1664991daeccfb14b8ad762420eef188d6
Syrent <Syrent2356@Gmail.com>
2024-07-04 15:20:40 +0330

    build(deps): update xseries

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.1.15"
+    version = "1.1.16"
     description = "A modular Kotlin library for Minecraft: JE"
 
     plugins.apply("maven-publish")
