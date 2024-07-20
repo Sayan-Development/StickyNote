3e98273013db4c68762c24a2e467dd113ff166d5
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-07-20 23:17:03 +0330

    chore(bukkit/nms): add NPCType

--- stickynote-bukkit/stickynote-bukkit-nms/src/main/kotlin/org/sayandev/stickynote/bukkit/nms/npc/NPCType.kt
+++ stickynote-bukkit/stickynote-bukkit-nms/src/main/kotlin/org/sayandev/stickynote/bukkit/nms/npc/NPCType.kt
@@ -1,4 +1,132 @@
 package org.sayandev.stickynote.bukkit.nms.npc
 
-class NPCType {
+import org.bukkit.entity.EntityType
+import org.sayandev.stickynote.nms.accessors.EntityTypeAccessor
+import java.lang.reflect.Field
+import java.util.*
+
+enum class NPCType {
+    AREA_EFFECT_CLOUD,
+    ARMOR_STAND,
+    ARROW,
+    BAT,
+    BEE,
+    BLAZE,
+    BOAT,
+    CAT,
+    CAVE_SPIDER,
+    CHICKEN,
+    COD,
+    COW,
+    CREEPER,
+    DOLPHIN,
+    DONKEY,
+    DRAGON_FIREBALL,
+    DROWNED,
+    ELDER_GUARDIAN,
+    END_CRYSTAL,
+    ENDER_DRAGON,
+    ENDERMAN,
+    EVOKER,
+    EXPERIENCE_ORB,
+    EYE_OF_ENDER,
+    FALLING_BLOCK,
+    FIREWORK_ROCKET,
+    FISHING_BOBBER,
+    FOX,
+    GHAST,
+    GIANT,
+    GUARDIAN,
+    HOGLIN,
+    HORSE,
+    HUSK,
+    ILLUSIONER,
+    IRON_GOLEM,
+    ITEM,
+    ITEM_FRAME,
+    FIREBALL,
+    LIGHTNING_BOLT,
+    LLAMA,
+    LLAMA_SPIT,
+    MAGMA_CUBE,
+    MINECART,
+    CHEST_MINECART,
+    COMMAND_BLOCK_MINECART,
+    FURNACE_MINECART,
+    HOPPER_MINECART,
+    SPAWNER_MINECART,
+    TNT_MINECART,
+    MULE,
+    MOOSHROOM,
+    OCELOT,
+    PAINTING,
+    PANDA,
+    PARROT,
+    PHANTOM,
+    PIG,
+    PIGLIN,
+    PIGLIN_BRUTE,
+    PILLAGER,
+    TNT,
+    PUFFERFISH,
+    RABBIT,
+    RAVAGER,
+    SALMON,
+    SHEEP,
+    SHULKER,
+    SHULKER_BULLET,
+    SILVERFISH,
+    SKELETON,
+    SKELETON_HORSE,
+    SLIME,
+    SMALL_FIREBALL,
+    SNOW_GOLEM,
+    SNOWBALL,
+    SPECTRAL_ARROW,
+    SPIDER,
+    SQUID,
+    STRAY,
+    STRIDER,
+    EGG,
+    ENDER_PEARL,
+    EXPERIENCE_BOTTLE,
+    POTION,
+    TRIDENT,
+    TRADER_LLAMA,
+    TROPICAL_FISH,
+    TURTLE,
+    VEX,
+    VILLAGER,
+    VINDICATOR,
+    WANDERING_TRADER,
+    WITCH,
+    WITHER,
+    WITHER_SKELETON,
+    WITHER_SKULL,
+    WOLF,
+    ZOGLIN,
+    ZOMBIE,
+    ZOMBIE_HORSE,
+    ZOMBIE_VILLAGER,
+    ZOMBIFIED_PIGLIN,
+    PLAYER;
+
+    val nmsEntityType: Any = (EntityTypeAccessor::class.java.getMethod("getField" + name.uppercase()).invoke(null) as Field).get(null)
+
+    companion object {
+        @JvmStatic
+        fun getByEntityType(entityType: EntityType): NPCType {
+            return when (entityType) {
+                EntityType.SPLASH_POTION -> POTION
+                EntityType.DROPPED_ITEM -> ITEM
+                EntityType.FISHING_HOOK -> FISHING_BOBBER
+                else -> valueOf(entityType.toString())
+            }
+        }
+
+        fun EntityType.toNPCType(): NPCType {
+            return getByEntityType(this)
+        }
+    }
+
 }
