29643f568a547937390f67c08dbd29bcc0c88082
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-07-24 15:32:09 +0330

    [ci skip] chore(bukkit/nms): add bukkit equipment slot

--- stickynote-bukkit/stickynote-bukkit-nms/src/main/kotlin/org/sayandev/stickynote/bukkit/nms/enum/EquipmentSlot.kt
+++ stickynote-bukkit/stickynote-bukkit-nms/src/main/kotlin/org/sayandev/stickynote/bukkit/nms/enum/EquipmentSlot.kt
@@ -2,11 +2,11 @@ package org.sayandev.stickynote.bukkit.nms.enum
 
 import org.sayandev.stickynote.nms.accessors.EquipmentSlotAccessor
 
-enum class EquipmentSlot(val nmsSlot: Any) {
-    MAINHAND(EquipmentSlotAccessor.FIELD_MAINHAND!!),
-    OFFHAND(EquipmentSlotAccessor.FIELD_OFFHAND!!),
-    HEAD(EquipmentSlotAccessor.FIELD_HEAD!!),
-    CHEST(EquipmentSlotAccessor.FIELD_CHEST!!),
-    FEET(EquipmentSlotAccessor.FIELD_FEET!!),
-    LEGS(EquipmentSlotAccessor.FIELD_LEGS!!)
+enum class EquipmentSlot(val bukkitSlot: org.bukkit.inventory.EquipmentSlot, val nmsSlot: Any) {
+    MAINHAND(org.bukkit.inventory.EquipmentSlot.HAND, EquipmentSlotAccessor.FIELD_MAINHAND!!),
+    OFFHAND(org.bukkit.inventory.EquipmentSlot.OFF_HAND, EquipmentSlotAccessor.FIELD_OFFHAND!!),
+    HEAD(org.bukkit.inventory.EquipmentSlot.HEAD, EquipmentSlotAccessor.FIELD_HEAD!!),
+    CHEST(org.bukkit.inventory.EquipmentSlot.CHEST, EquipmentSlotAccessor.FIELD_CHEST!!),
+    FEET(org.bukkit.inventory.EquipmentSlot.FEET, EquipmentSlotAccessor.FIELD_FEET!!),
+    LEGS(org.bukkit.inventory.EquipmentSlot.LEGS, EquipmentSlotAccessor.FIELD_LEGS!!)
 }
