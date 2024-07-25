e7c6a558f927cbff41426b4791bd8399f5bd0bcf
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-07-25 19:03:06 +0330

    release 1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.3.4"
+    version = "1.3.5"
     description = "A modular Kotlin library for Minecraft: JE"
 
     plugins.apply("maven-publish")
