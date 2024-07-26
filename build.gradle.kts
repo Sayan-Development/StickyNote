3a08eb4f4ba11ca5a62cf85c6e351a30b712245c
Syrent <Syrent2356@Gmail.com>
2024-07-26 19:44:56 +0330

    1.3.5 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.3.3"
+    version = "1.3.5"
     description = "A modular Kotlin library for Minecraft: JE"
 
     plugins.apply("maven-publish")
