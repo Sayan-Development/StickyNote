ac6218547ac3a38cc20e14474a93d8aa5682ada0
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-08-11 22:50:58 +0330

    chore(bukkit/nms): add ItemDisplayContext enum

---
+++ stickynote-bukkit/stickynote-bukkit-nms/src/main/kotlin/org/sayandev/stickynote/bukkit/nms/enum/ItemDisplayContext.kt
@@ -0,0 +1,15 @@
+package org.sayandev.stickynote.bukkit.nms.enum
+
+import org.sayandev.stickynote.bukkit.nms.accessors.ItemDisplayContextAccessor
+
+enum class ItemDisplayContext(val nmsObject: Any) {
+    NONE(ItemDisplayContextAccessor.FIELD_NONE!!),
+    THIRD_PERSON_LEFT_HAND(ItemDisplayContextAccessor.FIELD_THIRD_PERSON_LEFT_HAND!!),
+    THIRD_PERSON_RIGHT_HAND(ItemDisplayContextAccessor.FIELD_THIRD_PERSON_RIGHT_HAND!!),
+    FIRST_PERSON_LEFT_HAND(ItemDisplayContextAccessor.FIELD_FIRST_PERSON_LEFT_HAND!!),
+    FIRST_PERSON_RIGHT_HAND(ItemDisplayContextAccessor.FIELD_FIRST_PERSON_RIGHT_HAND!!),
+    HEAD(ItemDisplayContextAccessor.FIELD_HEAD!!),
+    GUI(ItemDisplayContextAccessor.FIELD_GUI!!),
+    GROUND(ItemDisplayContextAccessor.FIELD_GROUND!!),
+    FIXED(ItemDisplayContextAccessor.FIELD_FIXED!!)
+}
