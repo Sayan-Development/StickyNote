6a5d5a8212e1704e89179bd2c306de193679cbd8
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-08-13 02:55:54 +0330

    chore(bukkit/nms): implement ItemDisplayNPC

---
+++ stickynote-bukkit/stickynote-bukkit-nms/src/main/kotlin/org/sayandev/stickynote/bukkit/nms/npc/entity/display/ItemDisplayNPC.kt
@@ -0,0 +1,41 @@
+package org.sayandev.stickynote.bukkit.nms.npc.entity.display
+
+import org.bukkit.Location
+import org.bukkit.inventory.ItemStack
+import org.sayandev.stickynote.bukkit.nms.NMSUtils.toNmsItemStack
+import org.sayandev.stickynote.bukkit.nms.accessors.Display_ItemDisplayAccessor
+import org.sayandev.stickynote.bukkit.nms.accessors.ItemDisplayContextAccessor
+import org.sayandev.stickynote.bukkit.nms.accessors.SynchedEntityDataAccessor
+import org.sayandev.stickynote.bukkit.nms.npc.NPCType
+
+class ItemDisplayNPC(
+    location: Location,
+    item: ItemStack
+): DisplayNPC(
+    location,
+    NPCType.ITEM_DISPLAY
+) {
+
+    var item: ItemStack = item
+        set(value) {
+            field = value
+
+            SynchedEntityDataAccessor.METHOD_SET!!.invoke(
+                getEntityData(),
+                Display_ItemDisplayAccessor.FIELD_DATA_ITEM_STACK_ID,
+                value.toNmsItemStack()
+            )
+        }
+
+    var itemDisplayContext: ItemDisplayContext = ItemDisplayContext.NONE
+        set(value) {
+            field = value
+
+            SynchedEntityDataAccessor.METHOD_SET!!.invoke(
+                getEntityData(),
+                Display_ItemDisplayAccessor.FIELD_DATA_ITEM_DISPLAY_ID,
+                value.getId()
+            )
+        }
+
+}
