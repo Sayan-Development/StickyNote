649a7ea5c09e2fcc3a5d31494e96d11384e68970
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-09-25 21:51:44 +0330

    change CompletableDeferred extension to Deferred

--- stickynote-core/src/main/kotlin/org/sayandev/stickynote/core/utils/CoroutineUtils.kt
+++ stickynote-core/src/main/kotlin/org/sayandev/stickynote/core/utils/CoroutineUtils.kt
@@ -1,11 +1,12 @@
 package org.sayandev.stickynote.core.utils
 
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.withTimeout
 
 object CoroutineUtils {
-    suspend fun <T> CompletableDeferred<T>.awaitWithTimeout(timeout: Long, onTimeout: (TimeoutCancellationException) -> Unit = {}): T? {
+    suspend fun <T> Deferred<T>.awaitWithTimeout(timeout: Long, onTimeout: (TimeoutCancellationException) -> Unit = {}): T? {
         return try {
             withTimeout(timeout) {
                 await()
