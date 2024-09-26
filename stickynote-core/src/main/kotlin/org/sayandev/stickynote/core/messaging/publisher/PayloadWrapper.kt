6373e4da7e134c54178503aa53ef75823095b3a6
Syrent <Syrent2356@Gmail.com>
2024-09-26 18:22:08 +0330

    feat: add type adapters to PayloadWrapper

--- stickynote-core/src/main/kotlin/org/sayandev/stickynote/core/messaging/publisher/PayloadWrapper.kt
+++ stickynote-core/src/main/kotlin/org/sayandev/stickynote/core/messaging/publisher/PayloadWrapper.kt
@@ -1,7 +1,9 @@
 package org.sayandev.stickynote.core.messaging.publisher
 
 import com.google.gson.Gson
-import java.util.UUID
+import com.google.gson.GsonBuilder
+import com.google.gson.TypeAdapter
+import java.util.*
 
 data class PayloadWrapper<P>(
     val uniqueId: UUID = UUID.randomUUID(),
@@ -9,7 +11,7 @@ data class PayloadWrapper<P>(
     val state: State = State.FORWARD,
     var source: String? = null,
     val target: String? = null,
-    val excludeSource: Boolean = false
+    val excludeSource: Boolean = false,
 ) {
     constructor(payload: P, state: State = State.FORWARD): this(UUID.randomUUID(), payload, state)
 
@@ -20,16 +22,35 @@ data class PayloadWrapper<P>(
     }
 
     companion object {
+        var gson = Gson()
+        val typeAdapters = mutableMapOf<Class<*>, TypeAdapter<*>>()
+
+        fun registerAdapter(type: Class<*>, adapter: TypeAdapter<*>) {
+            typeAdapters[type] = adapter
+        }
+
+        fun unregisterAdapter(type: Class<*>) {
+            typeAdapters.remove(type)
+        }
+
+        fun updateGson() {
+            val builder = GsonBuilder()
+            for ((clazz, adapter) in typeAdapters) {
+                builder.registerTypeAdapter(clazz, adapter)
+            }
+            gson = builder.create()
+        }
+
         fun PayloadWrapper<*>.asJson(): String {
-            return Gson().toJson(this, PayloadWrapper::class.java)
+            return gson.toJson(this, PayloadWrapper::class.java)
         }
 
         fun <P> String.asPayloadWrapper(): PayloadWrapper<P> {
-            return Gson().fromJson<PayloadWrapper<P>>(this, PayloadWrapper::class.java)
+            return gson.fromJson<PayloadWrapper<P>>(this, PayloadWrapper::class.java)
         }
 
         fun <P> PayloadWrapper<*>.typedPayload(payloadClass: Class<P>): P {
-            return Gson().fromJson(Gson().toJson(this.payload), payloadClass)
+            return gson.fromJson(Gson().toJson(this.payload), payloadClass)
         }
     }
 }
