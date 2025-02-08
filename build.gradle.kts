a0aa0a25f18b64e04252d445f4f45dda7b76a7b4
Syrent <syrent2356@gmail.com>
2025-02-08 15:26:51 +0330

    build: 1.8.9.16 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.8.9.13"
+    version = "1.8.9.16"
     description = "A modular Kotlin framework for Minecraft: JE"
 
     plugins.apply("maven-publish")
