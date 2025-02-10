a57b5a3e1ca766cb991839339c2e7a541f41357a
Syrent <syrent2356@gmail.com>
2025-02-10 12:55:19 +0330

    [publish] build: release 1.8.9.37

--- stickynote-core/src/main/kotlin/org/sayandev/stickynote/core/configuration/UUIDSerializer.kt
+++ stickynote-core/src/main/kotlin/org/sayandev/stickynote/core/configuration/UUIDSerializer.kt
@@ -6,18 +6,11 @@ import java.lang.reflect.Type
 import java.util.*
 
 class UUIDSerializer : TypeSerializer<UUID> {
-    override fun deserialize(
-        type: Type,
-        node: ConfigurationNode
-    ): UUID {
+    override fun deserialize(type: Type, node: ConfigurationNode): UUID {
         return UUID.fromString(node.string)
     }
 
-    override fun serialize(
-        type: Type,
-        obj: UUID?,
-        node: ConfigurationNode
-    ) {
+    override fun serialize(type: Type, obj: UUID?, node: ConfigurationNode) {
         node.set(obj!!.toString())
     }
 }
