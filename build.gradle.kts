06482446c733de6d01995bbca45bfbe6d6ace246
Syrent <syrent2356@gmail.com>
2025-02-20 00:16:55 +0330

    fix: update RedisPublisher to use local Jedis resource and close connections properly

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.8.9.82"
+    version = "1.8.9.85"
     description = "A modular Kotlin framework for Minecraft: JE"
 
     plugins.apply("maven-publish")
