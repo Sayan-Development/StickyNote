d92c236a42bd3128698954aa2e2e829686da1207
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2025-02-23 13:46:13 +0330

    [publish] build 1.8.9.96

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.8.9.95"
+    version = "1.8.9.96"
     description = "A modular Kotlin framework for Minecraft: JE"
 
     plugins.apply("maven-publish")
