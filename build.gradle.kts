99bc78c9147057fc479d6e5bf26460a5533dabb3
Syrent <syrent2356@gmail.com>
2025-03-10 03:41:11 +0330

    [publish] chore: use coroutine core jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "org.sayandev"
-    version = "1.8.9.104"
+    version = "1.8.9.105"
     description = "A modular Kotlin framework for Minecraft: JE"
 
     plugins.apply("maven-publish")
